b8b31b05681042ef77402394fbbe590711cd2646
viettrungIT3 <viettrungcntt03@gmail.com>
2022-07-29 09:46:30 +0700

    get list employees

---
+++ Springboot-jpa-crud-example/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @GetMapping()
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+}
