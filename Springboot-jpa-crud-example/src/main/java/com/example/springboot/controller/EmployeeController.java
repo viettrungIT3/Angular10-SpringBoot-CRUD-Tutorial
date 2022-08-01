ff00b28c5a79e813a3e7022d474afccd6210d048
viettrungIT3 <viettrungcntt03@gmail.com>
2022-08-01 09:54:23 +0700

    11 - Connecting Angular with List Employee REST API

--- Springboot-jpa-crud-example/src/main/java/com/example/springboot/controller/EmployeeController.java
+++ Springboot-jpa-crud-example/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -5,12 +5,14 @@ import com.example.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
--- angular10-springboot-client/src/app/employee-list/employee-list.component.ts
+++ angular10-springboot-client/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from "../employee";
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,25 +9,18 @@ import { Employee } from "../employee";
 })
 export class EmployeeListComponent implements OnInit {
 
-  employees : Employee[] | undefined;
+  employees: Employee[] | undefined;
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [
-      {
-        "id": 1,
-        "firstName": "Trung",
-        "lastName": "Nguyễn",
-        "email": "viettrungcntt03@gmail.com"
-      },
-      {
-        "id": 2,
-        "firstName": "Trung 2",
-        "lastName": "Nguyễn",
-        "email": "viettrunga0@gmail.com"
-      }
-    ]
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    })
   }
 
 }
