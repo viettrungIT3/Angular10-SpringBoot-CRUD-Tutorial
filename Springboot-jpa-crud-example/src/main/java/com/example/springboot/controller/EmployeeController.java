fa51e935b7f8360cbd9e58a8414eef3805adf252
viettrungIT3 <viettrungcntt03@gmail.com>
2022-08-01 15:21:52 +0700

    17 - Creating REST API to Get Employee by ID

--- Springboot-jpa-crud-example/src/main/java/com/example/springboot/controller/EmployeeController.java
+++ Springboot-jpa-crud-example/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.exception.ResourceNotFoundException;
 import com.example.springboot.model.Employee;
 import com.example.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +33,14 @@ public class EmployeeController {
     @PostMapping("/all")
     public List<Employee> createEmployees(@RequestBody List<Employee> employee) {
         return repository.saveAll(employee);
+    }
+
+    // get Employee by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) throws ResourceNotFoundException {
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok().body(employee);
     }
 
 }
